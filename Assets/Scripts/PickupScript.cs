8218d73a0d34bdb8ccbea4c56823a07f6b1d234f
encuica <47792480+encuica@users.noreply.github.com>
2019-02-26 11:39:34 +0800

    190226 EC Minor Edits
    Just shifting pickup script to new file than within the movement script. (wowee such redundancy)

---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{	
+
+	public int northCount;
+	public int southCount;
+	public int eastCount;
+	public int westCount;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		northCount = 0;
+		southCount = 0;
+		eastCount = 0;
+		westCount = 0;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	// EC edited this: Pickup everything that enters the trigger, and set such that last touched object is only one which matters
+	// iswear there is an easier way but okay i shouldn't overcomplicate things
+    void OnTriggerEnter(Collider other)
+    {
+		if(other.gameObject.CompareTag("PickupNorth")){
+			other.gameObject.SetActive(false);
+			northCount += 1;
+			southCount = 0;
+			eastCount = 0;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupSouth")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount += 1;
+			eastCount = 0;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupEast")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount = 0;
+			eastCount += 1;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupWest")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount = 0;
+			eastCount = 0;
+			westCount += 1;
+		}
+    }
+}
--- Assets/Scripts/PlayerShipMovement.cs
+++ Assets/Scripts/PlayerShipMovement.cs
@@ -25,19 +25,10 @@ public class PlayerShipMovement : MonoBehaviour
     private Quaternion testLerpRotation;
 
     //private int _layerMask = 1 << 4;
-	
-	public int northCount;
-	public int southCount;
-	public int eastCount;
-	public int westCount;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-		northCount = 0;
-		southCount = 0;
-		eastCount = 0;
-		westCount = 0;
     }
 
     void Update()
@@ -112,39 +103,5 @@ public class PlayerShipMovement : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, seaRollingRotation, Time.deltaTime / 0.15f);
         */
 
-    }
-	
-	// EC edited this: Pickup everything that enters the trigger, and set such that last touched object is only one which matters
-	// iswear there is an easier way but okay i shouldn't overcomplicate things
-    void OnTriggerEnter(Collider other)
-    {
-		if(other.gameObject.CompareTag("PickupNorth")){
-			other.gameObject.SetActive(false);
-			northCount += 1;
-			southCount = 0;
-			eastCount = 0;
-			westCount = 0;
-		}
-		else if(other.gameObject.CompareTag("PickupSouth")){
-			other.gameObject.SetActive(false);
-			northCount = 0;
-			southCount += 1;
-			eastCount = 0;
-			westCount = 0;
-		}
-		else if(other.gameObject.CompareTag("PickupEast")){
-			other.gameObject.SetActive(false);
-			northCount = 0;
-			southCount = 0;
-			eastCount += 1;
-			westCount = 0;
-		}
-		else if(other.gameObject.CompareTag("PickupWest")){
-			other.gameObject.SetActive(false);
-			northCount = 0;
-			southCount = 0;
-			eastCount = 0;
-			westCount += 1;
-		}
     }
 }
