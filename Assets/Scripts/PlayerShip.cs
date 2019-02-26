ef55c6e5df41dc37735c5821c4247b23b049c531
unknown <chinkeeyong@gmail.com>
2019-02-26 15:35:37 +0800

    26-02-2019 fixed player ship listing at 0 velocity

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -28,16 +28,11 @@ public class PlayerShip : MonoBehaviour
     public int collisionDamageBase;
     public int collisionDamageMultiplier;
 
-    private int health;
     private int terrainLayer;
+    private int health;
     private float timeToNextHealth;
 
     private Rigidbody rb;
-    private Quaternion seaRollingRotation;
-    private Quaternion targetRotation;
-    private Quaternion testLerpRotation;
-
-    //private int _layerMask = 1 << 4;
 
     void Start()
     {
@@ -60,15 +55,14 @@ public class PlayerShip : MonoBehaviour
         // Apply input movement to the rigidbody
         rb.AddForce(Wind.velocity * forwardSpeed);
 
-        // Get target rotation from the rigidbody's current velocity
-        targetRotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
-
-        // Turn toward the target rotation
-        if (rb.velocity.magnitude > 0)
+        // While moving, turn toward the target rotation and tilt left or right
+        if (rb.velocity.magnitude > 0.001F)
         {
+            // Get target rotation from the rigidbody's current velocity
+            Quaternion targetRotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+
             // We first lerp the boat toward the rotation. If the turn angle is too large then we discard the lerp and cap it at the max turn speed.
-            // This also gives us the turn angle to play with later.
-            testLerpRotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnLerpAmount);
+            Quaternion testLerpRotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnLerpAmount);
             float turnAngle = Quaternion.Angle(transform.rotation, testLerpRotation);
             if (turnAngle < maxTurnSpeed)
             {
@@ -78,6 +72,12 @@ public class PlayerShip : MonoBehaviour
             {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxTurnSpeed);
             }
+
+            // Get the angle between our current velocity and our current rotation
+            float angleToVelocity = Vector3.SignedAngle(transform.forward, rb.velocity, Vector3.up);
+
+            // Tilt left or right depending on the turn angle
+            transform.Rotate(Vector3.forward, angleToVelocity * Time.deltaTime * zTiltMagnitude);
         }
 
         // Get the y position from the water controller and translate us to it
@@ -86,13 +86,6 @@ public class PlayerShip : MonoBehaviour
         float yDelta = yPosition - transform.position.y;
         transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, yPosition, transform.position.z), Time.deltaTime);
 
-        // Tilt up or down depending on the y delta
-        transform.Rotate(Vector3.right * yDelta * xTiltMagnitude * Time.deltaTime * rb.velocity.magnitude);
-
-        // Get the angle between our current velocity and our current rotation
-        float angleToVelocity = Vector3.SignedAngle(transform.forward, rb.velocity, Vector3.up);
-        // Tilt left or right depending on the turn angle
-        transform.Rotate(Vector3.forward, angleToVelocity * Time.deltaTime * zTiltMagnitude);
         // Sanity check - lerp back to resting position, and cap at z tilt maximum
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.forward, Vector3.up), Time.deltaTime * zTiltLerpAmount);
         if (transform.eulerAngles.z > zTiltMaximum)
@@ -107,25 +100,8 @@ public class PlayerShip : MonoBehaviour
             }
         }
 
-        /*
-        // Raycast to the sea collider mesh and rotate to the normal
-        Vector3 raycastSource = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Vector3 theRay = -transform.up;
-        RaycastHit hit;
-        if (Physics.Raycast(raycastSource, theRay, out hit, 20, _layerMask))
-        {
-            Debug.DrawRay(raycastSource, theRay * hit.distance, Color.yellow);
-            Debug.DrawRay(raycastSource, hit.normal * 1000, Color.red);
-            print("Hit");
-            seaRollingRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-        }
-        else
-        {
-            Debug.DrawRay(raycastSource, theRay * 1000, Color.white);
-            print ("NoHit");
-        }
-        transform.rotation = Quaternion.Lerp(transform.rotation, seaRollingRotation, Time.deltaTime / 0.15f);
-        */
+        // Tilt up or down depending on the y delta
+        transform.Rotate(Vector3.right * yDelta * xTiltMagnitude * Time.deltaTime * rb.velocity.magnitude);
     }
 
     private void UpdateHealthText()
