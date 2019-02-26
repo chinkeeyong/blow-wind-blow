def0ec08bd24086cc14d28650829b4333ac050cc
encuica <47792480+encuica@users.noreply.github.com>
2019-02-26 11:24:37 +0800

    190226 EC Pickup Count
    Added northCount, southCount, etc variables to PlayerShipMovement (counts last picked up)

--- Assets/Scripts/PlayerShipMovement.cs
+++ Assets/Scripts/PlayerShipMovement.cs
@@ -25,12 +25,19 @@ public class PlayerShipMovement : MonoBehaviour
     private Quaternion testLerpRotation;
 
     //private int _layerMask = 1 << 4;
+	
+	public int northCount;
+	public int southCount;
+	public int eastCount;
+	public int westCount;
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody>();
-
+		northCount = 0;
+		southCount = 0;
+		eastCount = 0;
+		westCount = 0;
     }
 
     void Update()
@@ -107,11 +114,37 @@ public class PlayerShipMovement : MonoBehaviour
 
     }
 	
-	// EC edited this: Pickup everything that enters the trigger
+	// EC edited this: Pickup everything that enters the trigger, and set such that last touched object is only one which matters
+	// iswear there is an easier way but okay i shouldn't overcomplicate things
     void OnTriggerEnter(Collider other)
     {
-		if(other.gameObject.CompareTag("PickUp")){
+		if(other.gameObject.CompareTag("PickupNorth")){
 			other.gameObject.SetActive(false);
+			northCount += 1;
+			southCount = 0;
+			eastCount = 0;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupSouth")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount += 1;
+			eastCount = 0;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupEast")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount = 0;
+			eastCount += 1;
+			westCount = 0;
+		}
+		else if(other.gameObject.CompareTag("PickupWest")){
+			other.gameObject.SetActive(false);
+			northCount = 0;
+			southCount = 0;
+			eastCount = 0;
+			westCount += 1;
 		}
     }
 }
