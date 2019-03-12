7c4d9174050f729a677f88d959a851b26b5fa4de
encuica <47792480+encuica@users.noreply.github.com>
2019-03-12 15:10:36 +0800

    190312 EC ??
    I don't know what I changed

---
+++ Assets/EC's (Failing) Cutscene Tests/cutsceneTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ATTACH SCRIPT TO FLAG WITH SPHERE COLLIDER, AND TAG THE SHIP WITH PLAYER
+
+public class cutsceneTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter(Collider other)
+    {
+		if(other.gameObject.CompareTag("Player")){			
+			if(other.gameObject.GetComponent<PickupScript>().scoreCount == 1){
+				SceneManager.LoadScene("CutsceneTest", LoadSceneMode.Additive);
+			} else if (other.gameObject.GetComponent<PickupScript>().scoreCount == 2){
+				SceneManager.LoadScene("CutsceneTest2", LoadSceneMode.Additive);
+			} else if (other.gameObject.GetComponent<PickupScript>().scoreCount == 3){
+				//SceneManager.LoadScene("CutsceneTest3", LoadSceneMode.Additive);
+			} else if (other.gameObject.GetComponent<PickupScript>().scoreCount == 4){
+				//SceneManager.LoadScene("CutsceneTest4", LoadSceneMode.Additive);
+			}
+			
+			Time.timeScale = 0; //pauses main scene
+		}
+	}
+}
---
+++ Assets/Scripts/moveTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class moveTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(transform.position.x > 1200){
+			transform.Translate(Vector3.left * Time.deltaTime * 100, Camera.main.transform);
+		} else {
+			SceneManager.UnloadSceneAsync("CutsceneTest");//unload scene, which is obviously not working
+		}
+    }
+}
